28d260b400f308bb913dd40c9ac134a6b5f6e1f6
harshit_tewari <librahtiwari@gmail.com>
2018-08-10 04:58:52 +0530

    added solution to decision tree classifier ques

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -27,7 +27,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print("Training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = clf.predict(features_test)
+print("Testing time:",round(time()-t0,3), "s")
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test,pred)
+
+print("Accuracy is ",acc)
+
 
 #########################################################
-
+##ACCURACY for DCT with min_sample_split=40 -> 97.78%
 
